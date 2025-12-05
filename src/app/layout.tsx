67e8b5fd4c7ec3bbd63821d614335f29d2721a6d
Nikhil Kumar <nikhilshaw575@gmail.com>
2025-12-05 10:53:49 +0530

    changed name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Socail X",
+  title: "SocialX",
   description: "A modern social media application powered by Next.js",
 };
 
